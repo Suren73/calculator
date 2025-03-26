698469e7879b31996eecf2f926b1b1f7a5079e3d
Andrei Surkov <andronsurkov73@gmail.com>
2025-03-26 17:53:09 +0300

    edit App.js & App.module.css

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,24 @@
 import React, { useState } from "react";
-import styles from "./App.module.css";
+import styles from "./app.module.css";
 
-const App = () => {
+export const App = () => {
+	const [firstOperand, setFirstOperand] = useState("0");
+	const [nextOperand, setNextOperand] = useState("");
+	const [operator, setOperator] = useState("");
+	const [isResult, setIsResult] = useState(false);
+
+	let output = firstOperand + operator + nextOperand;
+
 	const calculator = (expr) => {
 		const tokens = expr.split(/([+\-*])/);
+
+		if (tokens.length === 0) return 0;
+
 		let currentResult = Number(tokens[0]);
 
 		for (let i = 1; i < tokens.length; i += 2) {
 			const operator = tokens[i];
-			const nextToken = Number(tokens[i + 1]);
-
+			const nextToken = Number(tokens[i + 1] || "0");
 			switch (operator) {
 				case "+":
 					currentResult += nextToken;
@@ -21,222 +30,93 @@ const App = () => {
 					break;
 			}
 		}
+
 		return currentResult;
 	};
 
-	const [currentValue, setCurrentValue] = useState("0");
-	const [expression, setExpression] = useState("");
-	const [isResult, setIsResult] = useState(false);
-
-	const getDisplayOutput = (expr, current) => {
-		const finalExpression = expr + current;
-		const result = calculator(finalExpression);
-
-		setCurrentValue(String(result));
-		setExpression("");
-		setIsResult(true);
-	};
-
-	const getReturn = () => {
-		return;
-	};
-
-	const checkLastChar = (expr, current) => {
-		let newExpression = expr + current;
-		const lastChar = newExpression.slice(-1);
-		const isLastChar = ["+", "-"].includes(lastChar);
-		return [newExpression, isLastChar];
-	};
-
-	const getNumber = (num) => {
-		setExpression("");
-		setCurrentValue(String(num));
-		setIsResult(false);
-	};
-
-	const getOperationTrue = (op) => {
-		setExpression(currentValue + op);
-		setCurrentValue("");
-		setIsResult(false);
-	};
-
-	const getOperationFalse = (op) => {
-		let [newExpression, isLastChar] = checkLastChar(
-			expression,
-			currentValue,
-		);
-
-		newExpression = isLastChar
-			? newExpression.slice(0, -1) + op
-			: newExpression + op;
-
-		setExpression(newExpression);
-		setCurrentValue("");
-	};
-
 	const handleNumber = (num) => {
-		isResult
-			? getNumber(num)
-			: setCurrentValue((prev) =>
-					prev === "0" ? String(num) : prev + num,
-				);
+		if (isResult) {
+			setIsResult(false);
+			setFirstOperand("0");
+			setOperator("");
+			setNextOperand("");
+		}
+		if (!operator) {
+			setFirstOperand((prev) => (prev === "0" ? num : prev + num));
+		} else {
+			setNextOperand((prev) => (prev === "0" ? num : prev + num));
+		}
 	};
 
-	const handleOperation = (op) => {
-		isResult ? getOperationTrue(op) : getOperationFalse(op);
+	const handleOperator = (op) => {
+		if (isResult) setIsResult(false);
+
+		if (nextOperand) {
+			setFirstOperand(output);
+			setNextOperand("");
+		}
+		setOperator(op);
 	};
 
 	const handleEquals = () => {
-		let [, isLastChar] = checkLastChar(expression, currentValue);
-		!isLastChar ? getDisplayOutput(expression, currentValue) : getReturn();
+		let expr = output;
+		if (operator && !nextOperand) {
+			expr += "0";
+		}
+		const total = calculator(expr);
+		setIsResult(true);
+		setFirstOperand(String(total));
+		setOperator("");
+		setNextOperand("");
 	};
 
 	const handleReset = () => {
-		setExpression("");
-		setCurrentValue("0");
+		setFirstOperand("0");
+		setOperator("");
+		setNextOperand("");
 		setIsResult(false);
 	};
 
-	// const NUMS = ["7", "8", "9", "4", "5", "6", "1", "2", "3", "0"];
-
-	const buttons = [
-		{
-			id: "9",
-			label: "9",
-			type: "number",
-			handler: handleNumber,
-			className: styles.button,
-		},
-		{
-			id: "8",
-			label: "8",
-			type: "number",
-			handler: handleNumber,
-			className: styles.button,
-		},
-		{
-			id: "7",
-			label: "7",
-			type: "number",
-			handler: handleNumber,
-			className: styles.button,
-		},
-		{
-			id: "6",
-			label: "6",
-			type: "number",
-			handler: handleNumber,
-			className: styles.button,
-		},
-		{
-			id: "5",
-			label: "5",
-			type: "number",
-			handler: handleNumber,
-			className: styles.button,
-		},
-		{
-			id: "4",
-			label: "4",
-			type: "number",
-			handler: handleNumber,
-			className: styles.button,
-		},
-		{
-			id: "3",
-			label: "3",
-			type: "number",
-			handler: handleNumber,
-			className: styles.button,
-		},
-		{
-			id: "2",
-			label: "2",
-			type: "number",
-			handler: handleNumber,
-			className: styles.button,
-		},
-		{
-			id: "1",
-			label: "1",
-			type: "number",
-			handler: handleNumber,
-			className: styles.button,
-		},
-		{
-			id: "0",
-			label: "0",
-			type: "number",
-			handler: handleNumber,
-			className: styles.button,
-		},
-		{
-			id: "C",
-			label: "C",
-			type: "operator",
-			handler: handleReset,
-			className: styles.reset,
-		},
-		{
-			id: "+",
-			label: "+",
-			type: "operator",
-			handler: handleOperation,
-			className: styles.operation,
-		},
-		{
-			id: "-",
-			label: "-",
-			type: "operator",
-			handler: handleOperation,
-			className: styles.operation,
-		},
-		{
-			id: "=",
-			label: "=",
-			type: "operator",
-			handler: handleEquals,
-			className: styles.equals,
-		},
-	];
+	const NUMS = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
 	return (
 		<div className={styles.app}>
 			<div
-				className={`${styles.display} ${isResult ? styles.result : ""}`}
+				className={`${styles.screen} ${isResult ? styles.result : ""}`}
 			>
-				{expression + currentValue}
+				{output || "0"}
 			</div>
 			<div className={styles.buttons}>
-				<div className={styles.numbers}>
-					{buttons.map(({ id, label, type, handler, className }) =>
-						type === "number" ? (
-							<button
-								key={id}
-								onClick={() => handler(label)}
-								className={`${className} ${label === "0" ? styles.zero : ""}`}
-							>
-								{label}
-							</button>
-						) : null,
-					)}
+				<div className={styles.buttonsNumbers}>
+					{NUMS.map((num) => (
+						<button
+							key={num}
+							className={`${styles.button} ${num === "0" ? styles.zero : ""}`}
+							onClick={() => handleNumber(num)}
+						>
+							{num}
+						</button>
+					))}
 				</div>
-				<div className={styles.operations}>
-					{buttons.map(({ id, label, type, handler, className }) =>
-						type === "operator" ? (
-							<div
-								key={id}
-								className={className}
-								onClick={
-									label === "+" || "-"
-										? () => handler(label)
-										: handler
-								}
-							>
-								{label}
-							</div>
-						) : null,
-					)}
+				<div className={styles.operators}>
+					<button className={styles.reset} onClick={handleReset}>
+						C
+					</button>
+					<button
+						className={styles.operator}
+						onClick={() => handleOperator("+")}
+					>
+						+
+					</button>
+					<button
+						className={styles.operator}
+						onClick={() => handleOperator("-")}
+					>
+						-
+					</button>
+					<button className={styles.equals} onClick={handleEquals}>
+						=
+					</button>
 				</div>
 			</div>
 		</div>
